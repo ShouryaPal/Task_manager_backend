4974290f45497358cc8bb656aa18e15af6fd246b
Shourya  Pal <104789024+ShouryaPal@users.noreply.github.com>
2023-06-14 13:03:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ const cors = require('cors');
 
 require('dotenv').config()
 
+
+const app = express();  
+const PORT = process.env.PORT || 5000 ; 
+
+app.use(express.json());
+app.use(cors());
+
 mongoose
 .connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
@@ -12,12 +19,6 @@ mongoose
 })
 .then(() => console.log('Connected TO MongoDB...'))
 .catch((err) => console.log(err))
-
-const app = express();  
-const PORT = process.env.PORT || 5000 ; 
-
-app.use(express.json());
-app.use(cors());
 
 app.use(router);
 
